935bf9c77e75c082063c9c4d5ccfd40408a4f1f7
Roberto E. M. Austin <0315108515@miutt.edu.mx>
2020-01-16 22:38:48 -0800

    Uso de hooks de react en ExerciceContainer

--- fitness/FitnessReact/src/pages/ExerciseContainer.js
+++ fitness/FitnessReact/src/pages/ExerciseContainer.js
@@ -1,43 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import Loading from '../components/Loading';
 import FatalError from './500';
 import Exercise from './Exercise';
 
-export default class ExerciseContainer extends Component{
 
-    state = {
-            data:[],
-            loading: true,
-            error: null
-    }
+export default () => {
+    const [ data, setData ] = useState([]);
+    const [ loading, setLoading ] = useState(true);
+    const [ error, setError ] = useState(null);
 
-    async componentDidMount(){
+    useEffect(() => {
+        const fetchExercises = async () => {
+            try{
+                let res = await fetch('http://localhost:8000/api/exercises');
+                let data = await res.json();
 
-        try{
-            let res = await fetch('http://localhost:8000/api/exercises');
-            let data = await res.json();
-            console.log(data);
-            
-            this.setState({
-                data,
-                loading: false
-            })
+                setData(data);
+                setLoading(false);
+            }
+            catch(error){
+                setError(error);
+                setLoading(false);
+            }
         }
-        catch(error){
-            this.setState({
-                loading: false,
-                error
-            })
-        }
-    }
+        fetchExercises();
+    }, []);
 
-    render(){
-        if(this.state.loading)
-            return <Loading/>
-        if(this.state.error)
-            return <FatalError/>
-        return(
-            <Exercise information={this.state.data}/>
-        )
+    if(loading){
+        return <Loading/>
     }
+    if(error){
+        return <FatalError/>
+    }
+    return(
+        <Exercise information={data}/>
+    )
 }
